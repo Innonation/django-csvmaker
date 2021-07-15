90ef2a98a1325ec28d4ada7fb064fe646b8c2649
gianluca-difranco <g.difranco@innonation.it>
2021-07-15 15:32:52 +0200

    Versione 1.1; Implementata la compatibilità con versioni minori di Django e .values()

--- csvmaker/utils.py
+++ csvmaker/utils.py
@@ -1,4 +1,6 @@
 import csv
+
+from django.forms import model_to_dict
 from django.http import HttpResponse
 
 def generate_csv_from_queryset(queryset, csv_name = "query_csv"):
@@ -9,20 +11,27 @@ def generate_csv_from_queryset(queryset, csv_name = "query_csv"):
     :return: oggetto response
     """
     try:
-        response = HttpResponse(
-            content_type='text/csv',
-            headers={'Content-Disposition': 'attachment; filename=' + csv_name + '.csv'},
-        )
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=' + csv_name + '.csv'
+
         model_field_names = []
         writer = csv.writer(response, delimiter=';')
-        for field in queryset.first()._meta.local_fields:
-            model_field_names.append(field.name)
+
+        if isinstance(queryset.first(), dict):
+            fields = queryset.first().keys()
+        else:
+            fields = [field.attname.split('_id')[0] if field.attname.endswith('_id') else field.attname for field in queryset.first()._meta.local_fields]
+
+        for field in fields:
+            model_field_names.append(field)
         writer.writerow(model_field_names)
 
         for query in queryset:
             csv_row = []
-            for field in queryset.first()._meta.local_fields:
-                csv_row.append(getattr(query, field.attname))
+            for field in fields:
+                csv_row.append(query[field] if isinstance(query, dict) else model_to_dict(query)[field])
+
             writer.writerow(csv_row)
 
         return response
